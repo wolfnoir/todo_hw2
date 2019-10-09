2d903d5e80d1f9a9773aebe270f3d3290f15416a
wolfnoir <amber.yang@stonybrook.edu>
2019-10-08 21:18:57 -0400

    test

--- src/components/list_screen/ListItemsTable.js
+++ src/components/list_screen/ListItemsTable.js
@@ -5,9 +5,11 @@ export class ListItemsTable extends Component {
     render() {
         return (
             <div id="list_items_container">
+                <div className="list_item_header_card">
                 <div className="list_item_task_header">Task</div>
                 <div className="list_item_due_date_header">Due Date</div>
                 <div className="list_item_status_header">Status</div>
+                </div>
                 {
                     this.props.todoList.items.map((todoItem)=>(
                         <ListItemCard 
